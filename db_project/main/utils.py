1c8273eeb7fce6ff5076bb7bf43cb144e0e21f24
Jonathan Math <jonathanamath@gmail.com>
2024-04-27 16:17:46 -0400

    rm confirm_location from utils

--- db_project/main/utils.py
+++ db_project/main/utils.py
@@ -2,8 +2,6 @@ import psycopg2
 from psycopg2 import Error
 from django.conf import settings
 
-from main.models import Block
-
 connection = psycopg2.connect(**settings.PSYCOPG2_CONN)
 
 cursor = connection.cursor()
@@ -11,8 +9,3 @@ cursor = connection.cursor()
 def run_query(sql_query):
     cursor.execute(sql_query)
     return cursor.fetchall()
-
-def confirm_location(block_id):
-    if Block.objects.get_member_count(block_id) <= 3:
-        return True
-    return False
