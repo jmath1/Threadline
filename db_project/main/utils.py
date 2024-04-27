a99fa2b9e490070a760b5a90c84ee9448f07fee2
Jonathan Math <jonathanamath@gmail.com>
2024-04-27 13:52:38 -0400

    add functionality for confirming a location

--- db_project/main/utils.py
+++ db_project/main/utils.py
@@ -2,10 +2,17 @@ import psycopg2
 from psycopg2 import Error
 from django.conf import settings
 
+from main.models import Block
+
 connection = psycopg2.connect(**settings.PSYCOPG2_CONN)
 
 cursor = connection.cursor()
 
-def query(sql_query):
+def run_query(sql_query):
     cursor.execute(sql_query)
     return cursor.fetchall()
+
+def confirm_location(block_id):
+    if Block.objects.get_member_count(block_id) <= 3:
+            return True
+    return False
