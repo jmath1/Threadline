59b16a2a72501075edaf27712c2956f98e93aeaa
Jonathan Math <jonathanamath@gmail.com>
2025-01-23 13:45:44 -0500

    move test runners and change to load GIS data rather than setting up db using SQL. This is the beginning of changing from raw SQL to ORM

---
+++ db_project/main/runners.py
@@ -0,0 +1,10 @@
+from django.test.runner import DiscoverRunner
+from django.core.management import call_command
+
+class GISDataTestRunner(DiscoverRunner):
+
+    def setup_databases(self, **kwargs):
+        # Force migrations during test setup
+        setup = super().setup_databases(**kwargs)
+        call_command("load_gis_data")
+        return setup
