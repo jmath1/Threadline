29403818fbdabbbf60de9b6ff82573655e90fb98
Jonathan Math <jonathanamath@gmail.com>
2025-01-23 13:42:32 -0500

    delete archive directory

--- db_project/db_project/archive/templatetags/map_tags.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.inclusion_tag('maps/init_map.html')
-def init_map(latitude=40.7831, longitude=-73.9712, zoom=13):
-    return {'latitude': latitude, 'longitude': longitude, 'zoom': zoom}
