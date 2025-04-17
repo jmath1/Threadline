ebc5bd1bd55da1baf9c4deb9c886ee22a3611052
Jonathan Math <jonathanamath@gmail.com>
2025-04-16 21:33:01 -0400

    update settings

--- db_project/db_project/settings.py
+++ db_project/db_project/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = "django-insecure-^a%1(5g9*c^k56jv(v8y5&fmzx83toi3p3u&qjw#$6x+r+z)^r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+
+ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
 
 AUTH_USER_MODEL = 'main.User'
@@ -164,6 +165,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
