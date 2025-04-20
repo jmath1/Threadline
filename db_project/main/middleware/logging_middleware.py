ba286174b85cbd4806e1f833a300c88ee5954f2c
Jonathan Math <jonathanamath@gmail.com>
2025-04-19 22:55:39 -0400

    make cleaner logging in middleware

--- db_project/main/middleware/logging_middleware.py
+++ db_project/main/middleware/logging_middleware.py
@@ -14,6 +14,10 @@ class LoggingMiddleware:
         response = self.get_response(request)
 
         # Log the outgoing response
-        logger.info(f"Response: {response.status_code} {response.content}")
+        content_lines = response.content.splitlines()
+        if len(content_lines) > 100:
+            logger.info(f"Response: {response.status_code} {content_lines[:100]} [Content truncated]")
+        else:
+            logger.info(f"Response: {response.status_code} {response.content}")
 
         return response
